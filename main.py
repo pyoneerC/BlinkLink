895494605245cf31350e676f05707d673f31504a
pyoneerC <maxcomperatutti@gmail.com>
2024-08-15 11:36:09 -0300

    postgres

--- main.py
+++ main.py
@@ -30,6 +30,13 @@ async def create_short_url(url: str):
 
             conn = get_db_connection()
             cursor = conn.cursor()
+            cursor.execute("SELECT * FROM urls WHERE short_code = %s", (code,))
+            existing_url = cursor.fetchone()
+            if existing_url is not None:
+                raise HTTPException(status_code=409, detail="Short code already exists: {}".format(code))
+
+            conn = get_db_connection()
+            cursor = conn.cursor()
             cursor.execute(
                 "INSERT INTO urls (short_code, original_url, created_at, last_updated_at, expiration_date, access_count) VALUES (%s, %s, %s, %s, %s, %s)",
                 (code, url, created_at, created_at, expiration_date, 0))
@@ -56,18 +63,39 @@ async def create_short_url(url: str):
 @app.get("/shorten/{short_code}")
 async def get_original_url(short_code: str):
     try:
-        if short_code not in urls:
+        conn = get_db_connection()
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM urls WHERE short_code = %s", (short_code,))
+        result = cursor.fetchone()
+        cursor.close()
+        conn.close()
+
+        if not result:
             raise HTTPException(status_code=404, detail="Short code not found")
 
-        if urls[short_code]["expiration_date"] < datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S %p"):
-            urls.pop(short_code)
+        expiration_date = result[5]
+        if expiration_date < datetime.datetime.now():
+            await delete_short_url(short_code)
             raise HTTPException(status_code=404, detail="Short code has expired")
 
-        urls[short_code]["access_count"] += 1
-        return urls[short_code]
+        conn = get_db_connection()
+        cursor = conn.cursor()
+        cursor.execute("UPDATE urls SET access_count = access_count + 1 WHERE short_code = %s", (short_code,))
+        conn.commit()
+        cursor.close()
+        conn.close()
 
-    except requests.exceptions.RequestException:
-        raise HTTPException(status_code=404, detail="An error occurred while getting the original URL, please try again later")
+        return {
+            "short_code": result[1],
+            "original_url": result[2],
+            "created_at": result[3],
+            "last_updated_at": result[4],
+            "expiration_date": result[5],
+            "access_count": result[6] + 1
+        }
+
+    except psycopg2.Error as e:
+        raise HTTPException(status_code=500, detail=f"Database error: {e}")
 
 
 @app.put("/shorten")
